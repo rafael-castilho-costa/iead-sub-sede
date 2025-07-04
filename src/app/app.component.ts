befef65e2b27ce737de9423d50b9500e4c4485d5
r_castilho <rafaelcastilhodacosta@gmail.com>
2025-07-04 18:08:43 -0300

    mudança de estrutura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { HeaderComponent } from "./core/components/header/header.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/views/home/home-routing.module.ts
@@ -0,0 +1,19 @@
+
+import { NgModule } from "@angular/core";
+import { HomeComponent } from "./home/home.component";
+import { RouterModule, Routes } from "@angular/router";
+
+  const routes:Routes = [
+
+        {
+          path:'',
+          component: HomeComponent,
+        }
+
+  ];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HomeRoutingModule { }
