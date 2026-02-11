4cefe6a26b007669362eb19362e9b7f02a64e540
r_castilho <rafaelcastilhodacosta@gmail.com>
2026-02-11 15:43:14 -0300

    feat(1-create-tab-donation): feat atvd #1

--- src/app/views/contribuicao/contribuicao/contribuicao.component.ts
+++ src/app/views/contribuicao/contribuicao/contribuicao.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-contribuicao',
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './contribuicao.component.html',
   styleUrl: './contribuicao.component.scss'
 })
 export class ContribuicaoComponent {
+  readonly pixKey = 'senadorcanedo@adgoiania.com.br';
+  readonly pixQrCodePath = '/assets/pix-qrcode.png';
+  qrCodeDisponivel = true;
+
+  copiarChavePix(): void {
+    if (!navigator?.clipboard?.writeText) {
+      return;
+    }
+
+    navigator.clipboard.writeText(this.pixKey);
+  }
+
+  tratarErroQrCode(): void {
+    this.qrCodeDisponivel = false;
+  }
 
 }
