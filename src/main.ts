1e1d7f4f30893d7c5f3cc27939d097c8d550bea3
r_castilho <rafaelcastilhodacosta@gmail.com>
2026-02-13 11:32:51 -0300

    fix(feat-analytics): fix

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { inject } from '@vercel/analytics';
 
 registerLocaleData(localePt, 'pt-BR');
+
+inject();
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
